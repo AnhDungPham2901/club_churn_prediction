96d5041e2a6e424f1f23dd171b30aab385d731b1
dungpa <dungpa@vng.com.vn>
2023-09-04 17:54:16 +0700

    Removed unnescessary files

--- configuration/.ipynb_checkpoints/paths-checkpoint.py
+++
@@ -1,4 +0,0 @@
-RAW_DATA = "/home/dungpa/club_churn_predictionn/data/raw"
-PROCESSED_DATA = "/home/dungpa/club_churn_predictionn/data/processed"
-MODEL_EMBEDED = "/home/dungpa/club_churn_predictionn/model/embeded"
-MODEL_RF = "/home/dungpa/club_churn_predictionn/model"
--- service/deployment/explainer/.ipynb_checkpoints/README-checkpoint.md
+++
@@ -1,2 +0,0 @@
-This work is done before the final model have been developed. That's why, we use the older one. For testing, you can use the provided list of churn customers ID to test:
-8163, 2586, 3204, 2189
